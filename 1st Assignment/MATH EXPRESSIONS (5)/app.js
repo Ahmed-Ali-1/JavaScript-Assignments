5db9043f0845d997ab080e975b3644905eee22c0
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-06-29 22:02:57 +0500

    MATH EXPRESSIONS

--- 1st Assignment/MATH EXPRESSIONS (5)/app.js
+++ 1st Assignment/MATH EXPRESSIONS (5)/app.js
@@ -1,6 +1,37 @@
 var num1 = 10;
 var num2 = 8;
 
-var addition = num1 - num2;
+var addition = num1 + num2;
+var substraction = num1 - num2;
+var multiplication = num1 * num2;
+var division = num1 / num2; 
+var modulus = num1 % num2; 
 
-document.write("Sum of " + num1 + " and " + num2 +"sum="+ addition);
+document.write("Sum of " + num1 + " and " + num2 + " is " + addition + "</br>");
+document.write("Sub of " + num1 + " and " + num2 + " is " + substraction + "</br>");
+document.write("Multiply of " + num1 + " and " + num2 + " is " + multiplication + "</br>");
+document.write("Divide of " + num1 + " and " + num2 + " is " + division + "</br>");
+document.write("Modulus of " + num1 + " and " + num2 + " is " + modulus + "</br>");
+
+
+var number;
+document.write("Value after variable declaration is: " + number + "</br>")
+
+var number = 5;
+document.write("Initial value: " + number + "</br>");
+
+number++;
+
+var number = 6 + 7;
+document.write("Value after addition is: " + number + "</br>");
+
+number--; 
+
+document.write("Value after decrement is: " + number + "<br>");
+
+var riminder = number % 12;
+document.write("Value after decrement is: " + riminder + "<br>");
+
+
+
+
