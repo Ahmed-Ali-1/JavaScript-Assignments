cfaa20489c758bb76ce25d32fdedb5aa951ec58e
Ahmed-Ali-1 <ahmedali034012449@gmail.com>
2025-08-05 20:42:36 +0500

    Add to more Chapter

---
+++ Assignments JS/IF…ELSE & ELSE IF STATEMENT, Chap(11, 13)/app.js
@@ -0,0 +1,85 @@
+// var userIput = prompt("Enter a Single Character");
+
+// var character = userIput.charAt(0);
+
+// var code = character.charCodeAt(0);
+
+// if (code >= 65 && code <= 90) {
+//   alert("You intered a Upper");
+// } else if (code >= 97 && code <= 122) {
+//   alert("You intered a lowercase");
+// } else if (code >= 48 && code <= 57) {
+//   alert("You intered a Number");
+// }
+
+// var num1 = +prompt("Enter the first number: ");
+// var num2 = +prompt("Enter the second number: ");
+
+// if (num1 > num2) {
+//   alert(num1 + " is larger than " + num2);
+// } else if (num1 == num2) {
+//   alert("You Entered both equal");
+// }
+
+// var userInp = +prompt("Enter a Number");
+
+// if (userInp > 0) {
+//   alert("The number is positive.");
+// } else if (userInp < 0) {
+//   alert("The number is negative.");
+// } else {
+//   alert("The number is zero.");
+// }
+
+// var userInp = prompt("Enter a Single Character: ").toLowerCase();
+
+// if (userInp.length === 1) {
+//   if (
+//     userInp === "a" ||
+//     userInp === "e" ||
+//     userInp === "i" ||
+//     userInp === "o" ||
+//     userInp === "u"
+//   ) {
+//     alert(true);
+//   } else {
+//     alert(false);
+//   }
+// }
+
+// var password = "Ahmed123";
+
+// var userInp = prompt("Enter Your Correct Password");
+
+// if (!userInp) {
+//   alert("Please enter your password");
+// } else if (userInp === password) {
+//   alert("Correct!..");
+// } else {
+//   alert("Incorrect Password");
+// }
+
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+// var time = prompt("Enter time in 24-hour format (e.g. 1900 for 7 PM):");
+
+// time = parseInt(time);
+
+// if (time >= 0 && time < 1200) {
+//   alert("Good Morning!");
+// } else if (time >= 1200 && time < 1700) {
+//   alert("Good Afternoon!");
+// } else if (time >= 1700 && time < 2100) {
+//   alert("Good Evening!");
+// } else if (time >= 2100 && time <= 2359) {
+//   alert("Good Night!");
+// } else {
+//   alert("Invalid time format! Please enter time between 0000 and 2359.");
+// }
--- Assignments JS/USER INPUT & CONDITIONAL 9-11/app.js
+++ Assignments JS/USER INPUT & CONDITIONAL 9-11/app.js
@@ -60,56 +60,45 @@
 //   alert("The cost equals");
 // }
 
-
-// if (true){ 
-// alert("True"); 
-// } 
-// if (false){ 
-// alert("False"); 
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
 // }
 
+// var subject1 = +prompt("Enter marks obtained in Subject 1:");
+// var subject2 = +prompt("Enter marks obtained in Subject 2:");
+// var subject3 = +prompt("Enter marks obtained in Subject 3:");
+// var totalMarks = +prompt("Enter total marks:");
 
+// var marksObtained = + subject1 + subject2 + subject3;
+// var per = (marksObtained / totalMarks) * 100;
 
-var subject1 = +prompt("Enter marks obtained in Subject 1:");
-var subject2 = +prompt("Enter marks obtained in Subject 2:");
-var subject3 = +prompt("Enter marks obtained in Subject 3:");
-var totalMarks = +prompt("Enter total marks:");
+// var grade, remarks;
 
+// if (per >= 80 && per <=100){
+//     grade = "A-One";
+//     remarks = "Excellent";
+// }else if(per >= 70 && per <=80){
+//     grade = "A";
+//     remarks = "Good";
+// }else if (per >=60 && per <=70){
+//     grade = "B";
+//     remarks = "You need to improve";
+// }else{
+//     grade = "Fail";
+//     remarks = "Supply Aaa Gai";
+// }
 
-var marksObtained = + subject1 + subject2 + subject3;
-var per = (marksObtained / totalMarks) * 100;
-
-var grade, remarks;
-
-
-if (per >= 80 && per <=100){
-    grade = "A-One";
-    remarks = "Excellent";
-}else if(per >= 70 && per <=80){
-    grade = "A";
-    remarks = "Good";
-}else if (per >=60 && per <=70){
-    grade = "B";
-    remarks = "You need to improve";
-}else{
-    grade = "Fail";
-    remarks = "Supply Aaa Gai";
-}
-
-document.write("<h2>Mark Sheet</h2></br>");
-document.write("Total Marks: " + totalMarks + "</br>");
-document.write("Marks Obtained: " + marksObtained + "</br>");
-document.write("Percentage: " + per + "%</br>");
-document.write("Grade: " + grade+ "</br>");
-document.write("Remarks: " + remarks+ "</br>");
-
-
-
-
-
+// document.write("<h2>Mark Sheet</h2></br>");
+// document.write("Total Marks: " + totalMarks + "</br>");
+// document.write("Marks Obtained: " + marksObtained + "</br>");
+// document.write("Percentage: " + per + "%</br>");
+// document.write("Grade: " + grade+ "</br>");
+// document.write("Remarks: " + remarks+ "</br>");
 
 // document.write("Computer: " + comp + "</br> Science: " + science + "</br> Maths: " + maths + "</br> English: " +eng+ "</br>");
-
 
 // var fullComp = 100;
 // var fullScience = 100;
@@ -124,3 +113,67 @@ document.write("Remarks: " + remarks+ "</br>");
 
 // document.write("Marks Obtained: " + marksObtained);
 
+// let secretNumber = 6;
+// var userGuess = +prompt("Guess the secret number (1 to 10");
+
+// if (userGuess === secretNumber) {
+//   alert("Bingo! Correct answer");
+// } else if (userGuess + 1 === secretNumber) {
+//   alert("Close enough to the correct answer");
+// } else if (userGuess - 1 === secretNumber) {
+//   alert("Close enough to the correct answer");
+// } else {
+//   alert("Try Again!");
+// }
+
+// var userInp = +prompt("Enter a Number: ");
+
+// if (userInp % 3 === 0) {
+//   alert("The number is divisible by 3");
+// } else {
+//   alert("The number is not divisible by 3");
+// }
+
+// var userInp = +prompt("Enter a Number: ");
+
+// if (userInp % 2 === 0) {
+//   alert("The number is EVEN");
+// } else {
+//   alert("The number is ODD");
+// }
+
+// var userTemp = +prompt("Enter Temperature");
+
+// if (userTemp > 40) {
+//   alert("It is too hot outside");
+// } else if (userTemp > 30) {
+//   alert("The Weather today is Normal.");
+// } else if (userTemp > 20) {
+//   alert("Todays Weather is cool.");
+// } else if (userTemp > 10) {
+//   alert("OMG! Today’s weather is so Cool.");
+// }
+
+// var num1 = parseFloat(prompt("Right a first number: "));
+// var num2 = parseFloat(prompt("Right a second number: "));
+// var opr = prompt(" Operation (+, -, *, /, %)");
+
+// var result;
+
+// if (opr === "+") {
+//   result = num1 + num2;
+// } else if (opr === "-") {
+//   result = num1 - num2;
+// } else if (opr === "*") {
+//   result = num1 * num2;
+// } else if (opr === "/") {
+//   result = num1 / num2;
+// } else if (opr === "%") {
+//   result = num1 % num2;
+// } else {
+//   alert("Invalid Operation");
+// }
+
+// if (result !== undefined) {
+//   alert("Result of" + num1 + " " + opr + " " + num2 + " " + "=" + " " + result);
+// }
